bae11d8391921c4f7035995aab4b36c12c94d61b
Maria Stelmakh <maniastelmah@gmail.com>
2023-11-01 22:09:03 +0400

    created test to check JUnit code on SoftAssertions page

---
+++ src/test/java/SelenideRepozitorySearch.java
@@ -0,0 +1,48 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideRepozitorySearch {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void searchJunitCodeOnSoftAssertsPage() {
+        String exampleJunit5 = String.join (
+                "\n"+ "@ExtendWith({SoftAssertsExtension.class})\n"
+                + "class Tests {\n"
+                + "@Test\n" + "void test() {\n"
+                + "Configuration.assertionMode = SOFT;\n"
+                + "open(\"page.html\");\n"
+                +"\n"
+                + "$(\"#first\").should(visible).click();\n"
+                + "$(\"#second\").should(visible).click();\n"
+                + "}\n"
+                + "}");
+
+        //Откройте страницу Selenide в Github
+        open("/selenide/selenide");
+        //Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $$(".js-wiki-sidebar-toggle-display li").shouldHave(itemWithText("SoftAssertions"));
+        $(".js-wiki-sidebar-toggle-display li button").click();
+        //Откройте страницу SoftAssertions
+        //$x("//a[text()='SoftAssertions']").click();
+        $(byTagAndText("a","SoftAssertions")).click();
+        //проверьте что внутри есть пример кода для JUnit5
+        $$("#user-content-1-using-testng-just-register-listener-bypass-annotation-for-test-class").shouldHave(texts(exampleJunit5));
+    }
+
+
+}
